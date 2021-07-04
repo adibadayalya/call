b2fa3c2243837bf69a2fa3e17bd8635299042d42
= <=>
2021-07-04 22:34:57 +0530

    turn

--- client/src/contexts/RoomContext.js
+++ client/src/contexts/RoomContext.js
@@ -127,8 +127,8 @@ export default function Room(props) {
                 },
                 {
                     urls: 'turn:numb.viagenie.ca',
-                    credential: 'muazkh',
-                    username: 'webrtc@live.com'
+                    credential: 'turnServerPassword',
+                    username: 'abadayalya@gmail.com'
                 },
             ]
         });
